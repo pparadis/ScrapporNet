18ee9c6e14ff9bdfed4184a88769f8ba9ac61017
Pascal Paradis <pascal.paradis@gmail.com>
2012-03-03 21:32:45 -0500

    First commit

---
+++ ScrapporNet/Fetch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HtmlAgilityPack;
+
+namespace ScrapporNet
+{
+    class Fetch
+    {
+        public Fetch()
+        {
+            
+        }
+
+        public void FetchWinePages()
+        {
+            HtmlDocument doc = new HtmlDocument();
+            doc.Load("http://www.saq.com/webapp/wcs/stores/servlet/CatalogSearchResultView?storeId=10001&catalogId=10001&resultCatEntryType=2&beginIndex=0&tri=RechercheUCIProdDescAttributeInfo&sensTri=AscOperator&searchType=100&viewTaskName=SAQCatalogSearchResultView&pageSize=100");
+        }
+    }
+}
---
+++ ScrapporNet/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Raven.Client.Document;
+
+namespace ScrapporNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var documentStore = new DocumentStore { Url = "http://pascal-pc:8080" };
+            documentStore.Initialize();
+
+            string wineid;
+
+            //using (var session = documentStore.OpenSession())
+            //{
+            //    var entity = new Wine { Name = "Allo" };
+
+            //    session.Store(entity);
+            //    session.SaveChanges();
+            //    wineid = entity.Id;
+                
+            //}
+
+            using (var session = documentStore.OpenSession())
+            {
+                //var entity = session.Load<Wine>(wineid);
+                //Console.WriteLine(entity.Name);
+                //session.SaveChanges();
+
+                foreach (var variable in session.Query<Wine>())
+                {
+                    Console.WriteLine(variable.Id);
+                }
+
+            }
+            Console.ReadLine();
+        }
+    }
+
+    public class Wine
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+
+    }
+}
