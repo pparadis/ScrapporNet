f4838d5b2533f0228de3e8378bf54e31d972b9e8
Pascal Paradis <pascal.paradis@gmail.com>
2012-03-04 19:51:57 -0500

    DTO

---
+++ ScrapporNet/Wine.cs
@@ -0,0 +1,15 @@
+﻿namespace ScrapporNet
+{
+    public class Wine
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public string Format { get; set; }
+
+        public override string ToString()
+        {
+            return Name + "-" + Id;
+        }
+    }
+}
