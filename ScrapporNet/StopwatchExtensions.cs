a9dde1e1ba4b6f683824d6f71fa694c678768b0a
Pascal Paradis <pascal.paradis@gmail.com>
2012-03-06 18:52:47 -0500

    Tests de performance en cours

---
+++ ScrapporNet/StopwatchExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace ScrapporNet
+{
+    public static class StopwatchExtensions
+    {
+        public static long Time(this Stopwatch sw, Action action, int iterations)
+        {
+            sw.Reset();
+            sw.Start();
+            for (var i = 0; i < iterations; i++)
+            {
+                action();
+            }
+            sw.Stop();
+
+            return sw.ElapsedMilliseconds;
+        }
+    }
+}
