8bb38e5c3f05ba6ccde994e642406de3977c8f06
Pascal Paradis <pascal.paradis@gmail.com>
2012-04-09 22:40:23 -0400

    Tests tests tests

---
+++ ParseTests/SearchPageTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ParseTests
+{
+    [TestClass]
+    class SearchPageTests
+    {
+        [TestMethod]
+        public void test1()
+        {
+            // Arrange
+            //HomeController controller = new HomeController();
+
+            // Act
+            //ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            //Assert.AreEqual("Welcome to ASP.NET MVC!", result.ViewBag.Message);
+        }
+
+    }
+}
